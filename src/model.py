38aeb2029ddc2e1053a18405e179b230ecd8b448
adarsh <adarshbellamane@gmail.com>
2026-01-25 12:41:28 +0530

    added weights and faiss

--- src/model.py
+++ src/model.py
@@ -1,5 +1,3 @@
-#Defining the neural network architecture
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,9 +8,9 @@ class EchoFindModel(nn.Module):
     def __init__(self, embedding_dim = 128):
         super().__init__()
 
-        # 1. LOAD BACKBONE (ResNet50 Upgrade)
-        weights = models.ResNet50_Weights.DEFAULT
-        self.backbone = models.resnet50(weights=weights)
+        # 1. LOAD BACKBONE (ResNet18)
+        weights = models.ResNet18_Weights.DEFAULT
+        self.backbone = models.resnet18(weights=weights)
 
         # 2. MODIFY INPUT LAYER (1 Channel)
         self.backbone.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
@@ -21,18 +19,16 @@ class EchoFindModel(nn.Module):
         self.backbone.fc = nn.Identity()
 
         # Projection Head
-        # ResNet50 (2048) -> 256 -> Embedding
+        # ResNet18 outputs 512 features (instead of 2048 in ResNet50)
+        # 512 -> 256 -> Embedding
         self.projection_head = nn.Sequential(
-            nn.Linear(2048, 512),
+            nn.Linear(512, 256),
             nn.ReLU(),
-            nn.Linear(512, embedding_dim)
+            nn.Linear(256, embedding_dim)
         )
 
     def forward(self, x):
         # x shape: (batch, 1, 128, 216)
-        
-        # We modified conv1 to take 1 channel, so we pass x directly.
-        # No need to repeat channels.
         
         # Get features from ResNet (Backbone)
         h = self.backbone(x)
